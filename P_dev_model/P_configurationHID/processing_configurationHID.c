6b9b482ec4b146a97eb003125652b8413cec8fc2
AlexGerasimchuk <ger_1987@ukr.net>
2018-05-22 01:19:18 +0300

    Added command processing

--- P_dev_model/P_configurationHID/processing_configurationHID.c
+++ P_dev_model/P_configurationHID/processing_configurationHID.c
@@ -28,37 +28,72 @@
 #define QUEUE_MESSAGE_SIZE  64
 
 QueueHandle_t inMessageQueue;
-uint8_t messageBuffer[QUEUE_MESSAGE_SIZE];
+uint8_t reqBuf[QUEUE_MESSAGE_SIZE];
+uint8_t respBuf[QUEUE_MESSAGE_SIZE];
 
 typedef enum
 {
 	COMUNICATION_GET_REG = (uint8_t)0x0,
 	COMUNICATION_SET_REG = (uint8_t)0x1,
 	COMUNICATION_RESET   = (uint8_t)0x2,
-}COMUNICATION_COMMAND;
+}REQ_COMMAND;
+
+typedef enum
+{
+	STATUS_RESP_OK = (uint8_t)0x0,
+	STATUS_RESP_ERROR = (uint8_t)0x1,
+}REQ_STATUS;
 
 #pragma pack(push,1)
 
-typedef struct
-{
-	uint8_t commandType;
-	uint8_t payload[];
-}headOfMessage_t;
+/****INPUT MESSAGE DESCRIPTION*******/
+
 
 typedef struct
 {
-	uint8_t commandType;
-	uint8_t payload[];
-}getRegComman_t;
+	uint16_t reAddress;
+	uint8_t  numberOfReg;
+}getRegReq_t;
+
+typedef struct
+{
+	uint16_t reAddress;
+	uint8_t  numberOfReg;
+	uint8_t  payload[];
+}setRegReq_t;
+
+typedef struct
+{
+	uint8_t reqType;
+	union
+	{
+		getRegReq_t getRegReq;
+		setRegReq_t setRegReq;
+	}payload;;
+}headOfReq_t;
+
+
+/****OUTPUT MESSAGE DESCRIPTION*******/
+typedef struct
+{
+	uint16_t reAddress;
+	uint8_t  numberOfReg;
+	uint8_t  payload[];
+}getRegResp_t;
+
+
+typedef struct
+{
+	uint8_t reqType;
+	uint8_t respStatus;
+	union
+	{
+		getRegResp_t getRegResp;
+	}payload;;
+}headOfRes_t;
 
 
 #pragma pack(pop)
-
-
-
-
-
-
 
 
 void rxDataCB(uint8_t epNumber, uint8_t numRx, uint8_t* rxData)
@@ -66,35 +101,52 @@ void rxDataCB(uint8_t epNumber, uint8_t numRx, uint8_t* rxData)
 	xQueueSendToBackFromISR(inMessageQueue, (void*)&rxData, 0);
 }
 
+
 void txDataComplete(void)
 {
 
 }
 
+void t_processing_configurationHID(void *in_Data) {
+	/*init USB HID */
+	usbHIDInit();
+	usbHIDAddRxCB(rxDataCB);
+	usbHIDAddTxCompleteCB(txDataComplete);
 
+	inMessageQueue = xQueueCreate(NUM_QUEUE_MESSAGE, QUEUE_MESSAGE_SIZE);
 
-void t_processing_configurationHID(void *in_Data)
-{
-    /*init USB HID */
-    usbHIDInit();
-    usbHIDAddRxCB(rxDataCB);
-    usbHIDAddTxCompleteCB(txDataComplete);
+	while (1) {
+		//Wait for receive message
+		xQueueReceive( inMessageQueue, (void*)reqBuf, portMAX_DELAY);
 
-    inMessageQueue = xQueueCreate(NUM_QUEUE_MESSAGE, QUEUE_MESSAGE_SIZE);
-
-	while(1){
-		xQueueReceive( inMessageQueue, (void*)&messageBuffer,  portMAX_DELAY);
-
-		switch( (((headOfMessage_t)(messageBuffer))->commandType)  )
-		{
-		case 	COMUNICATION_GET_REG: break;
-		case 	COMUNICATION_SET_REG:
-
-			    break;
-		case 	COMUNICATION_RESET:
-			    NVIC_SystemReset();
-			    break;
-		default: break;
+		switch ( ((headOfReq_t*) reqBuf)->reqType) {
+		case COMUNICATION_GET_REG:
+			((headOfRes_t*) respBuf)->reqType = COMUNICATION_GET_REG;
+			((headOfRes_t*) respBuf)->payload.getRegResp.numberOfReg = ((headOfReq_t*) reqBuf)->payload.getRegReq.numberOfReg;
+			((headOfRes_t*) respBuf)->payload.getRegResp.reAddress   = ((headOfReq_t*) reqBuf)->payload.getRegReq.reAddress;
+			((headOfRes_t*) respBuf)->respStatus = (MEM_ERROR == processing_mem_map_read_s_proces_object_modbus(
+									                                           (uint16_t*)((headOfRes_t*) respBuf)->payload.getRegResp.payload,
+									                                           (uint16_t) ((headOfReq_t*) reqBuf)->payload.getRegReq.reAddress,
+									                                           (uint16_t) ((headOfReq_t*) reqBuf)->payload.getRegReq.numberOfReg) )
+									                                           ? (STATUS_RESP_OK)
+									                                           : (STATUS_RESP_ERROR);
+			break;
+		case COMUNICATION_SET_REG:
+			((headOfRes_t*) respBuf)->reqType = COMUNICATION_GET_REG;
+			((headOfRes_t*) respBuf)->payload.getRegResp.numberOfReg = ((headOfReq_t*) reqBuf)->payload.getRegReq.numberOfReg;
+			((headOfRes_t*) respBuf)->payload.getRegResp.reAddress   = ((headOfReq_t*) reqBuf)->payload.getRegReq.reAddress;
+			((headOfRes_t*) respBuf)->respStatus = (MEM_ERROR == processing_mem_map_write_s_proces_object_modbus(
+									                                           (uint16_t*)((headOfRes_t*) reqBuf)->payload.getRegResp.payload,
+									                                           (uint16_t) ((headOfReq_t*) reqBuf)->payload.getRegReq.reAddress,
+									                                           (uint16_t) ((headOfReq_t*) reqBuf)->payload.getRegReq.numberOfReg) )
+									                                           ? (STATUS_RESP_OK)
+									                                           : (STATUS_RESP_ERROR);
+			break;
+		case COMUNICATION_RESET:
+			NVIC_SystemReset();
+			break;
+		default:
+			break;
 		}
 	}
 }
